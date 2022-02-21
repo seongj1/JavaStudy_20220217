cc1a3f9b976c30616d7b1816dae8f13089f4b6e6
ITPS <ITPS@gmail.com>
2022-02-21 12:49:43 +0900

    연산자 추가

---
+++ JavaStudy_7896/src/a03_연산자/Operation03.java
@@ -0,0 +1,20 @@
+package a03_연산자;
+
+public class Operation03 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		int result1 = num1 < num2 ? 1 : 2;
+		
+		System.out.println(result1);
+		
+		boolean result2 = num1 > num2 ? true : false;
+		
+		System.out.println(result2);
+		
+
+	}
+
+}
