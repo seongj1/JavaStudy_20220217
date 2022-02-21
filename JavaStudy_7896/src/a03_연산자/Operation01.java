fe718754a158ef0f266055e59fb7080da2bf6fc2
ITPS <ITPS@gmail.com>
2022-02-21 11:17:10 +0900

    연산자 수업내용

---
+++ JavaStudy_7896/src/a03_연산자/Operation01.java
@@ -0,0 +1,18 @@
+package a03_연산자;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		// 증(가)감(소)연산자
+		int score = 10;
+		//score = score + 1;
+		System.out.println(score++);
+		System.out.println(score--);
+		System.out.println(score);
+		
+		
+		
+
+	}
+
+}
