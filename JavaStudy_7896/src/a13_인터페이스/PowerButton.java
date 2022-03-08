c350a0f3ebf34ba5e6a47f39b717b775aeb95e05
ITPS <ITPS@gmail.com>
2022-03-08 12:58:50 +0900

    2022년 3월 8일 인터페이스 수업

---
+++ JavaStudy_7896/src/a13_인터페이스/PowerButton.java
@@ -0,0 +1,12 @@
+package a13_인터페이스;
+
+public interface PowerButton {
+	public void onClick();
+	public void sendSignal();
+	
+	
+	
+		
+	
+
+}
---
+++ JavaStudy_7896/src/a13_인터페이스/VolumeUpButton.java
@@ -0,0 +1,6 @@
+package a13_인터페이스;
+
+public interface VolumeUpButton {
+	public void keepTheUpButton();
+	
+}
